76dad1f1e5b2de8eca20409d7fa481e55994334d
joy <13164432553@163.com>
2018-08-04 15:26:36 +0800

    config int

--- spring-cloud-config-client-git-sample/src/main/java/org/liujk/spring/cloud/config/ConfigClientApplication.java
+++ spring-cloud-config-client-git-sample/src/main/java/org/liujk/spring/cloud/config/ConfigClientApplication.java
@@ -1,16 +1,24 @@
 package org.liujk.spring.cloud.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableConfigServer
+@RestController
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
+    @Value("${foo}")
+    private String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
 
 }
